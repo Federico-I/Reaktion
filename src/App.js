f78fbfba4f61ddd59e26d0e87887196835fe984a
Threxos <threxos.48@gmail.com>
2022-12-15 21:44:13 +0100

    First-Components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+"strict mode";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <>
+      <Header text="Hallo Welt" />
+      <div className="container">
+        <h1>Willkomen auch Reaktion</h1>
+      </div>
+    </>
+  );
+}
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+
+function Header(props) {
+  return (
+    <header>
+      <div className="container">
+        <h2>{props.text}</h2>
+      </div>
+    </header>
+  );
+}
+
+Header.defaultProps = {
+  text: "Reaktion",
+};
+
+Header.prpoTypes = {
+  text: PropTypes.string, // ex: isRequired
+};
+
+export default Header;
