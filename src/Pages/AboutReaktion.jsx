9c97185e369d240cf36ddb1cd39b179762f14d9a
Threxos <threxos.48@gmail.com>
2023-01-19 23:04:10 +0100

    reaktion-context-typo

--- src/Pages/AboutReaktion.jsx
+++ src/Pages/AboutReaktion.jsx
@@ -7,7 +7,7 @@ function AboutReaktion() {
     <Card>
       <div className="about">
         <h1>About this Project</h1>
-        <p>This is a React appto leave Feedback service.</p>
+        <p>This is a React app to leave Feedback service.</p>
         <p>Version: 1.0.1</p>
         <p>
           <Link to="/">Back to home Page</Link>
