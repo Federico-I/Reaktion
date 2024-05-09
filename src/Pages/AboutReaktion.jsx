f169c5bd715f896bd6c0a70b734c51f9e7ad3c6d
Federico-I <threxos.48@gmail.com>
2024-05-09 20:17:48 +0100

    index.js-bug-fixed

--- src/Pages/AboutReaktion.jsx
+++ src/Pages/AboutReaktion.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Card from "../Components/Shared/Card";
 import { Link } from "react-router-dom";
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 "strict mode";
-
 import { React } from "react";
 import { ReactDOM } from "react-dom";
 import "./index.css";
 import { App } from "./App.js";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
+
