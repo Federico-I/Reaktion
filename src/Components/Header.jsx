0728b345d148defd6a6e20cb74b70098c17b41c2
Federico-I <threxos.48@gmail.com>
2024-05-11 18:19:31 +0100

    headerCode-bug-fixed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Header(text, bgColor, textColor) {
   const headerStyles = {
@@ -9,7 +10,9 @@ function Header(text, bgColor, textColor) {
   return (
     <header style={headerStyles}>
       <div className="container">
-        <h2>{text}</h2>
+        <Link to="/" style={{ textDecoration: "none", color: "#ff6a95"}}>
+          <h2>{text}</h2>        
+        </Link>
       </div>
     </header>
   );
@@ -18,11 +21,11 @@ function Header(text, bgColor, textColor) {
 Header.defaultProps = {
   text: "Reaktion",
   bgColor: "rgba(0,0,0,0.4)",
-  textColor: "#ff6",
+  textColor: "#ff6a95",
 };
 
 Header.prpoTypes = {
-  text: PropTypes.string, // ex: isRequired
+  text: PropTypes.string,
   bgColor: PropTypes.string,
   textColor: PropTypes.string,
 };
